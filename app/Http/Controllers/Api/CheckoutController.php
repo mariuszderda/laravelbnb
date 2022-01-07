21777f572bc75b0fa551b7c0a2e0cd53e6bf36f7
Mariusz Derda <mariusz.derda@gmail.com>
2022-01-07 20:33:39 +0100

    Add Checkout controller

---
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Address;
+use App\Bookable;
+use App\Booking;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function __invoke(Request $request)
+    {
+        $data = $request->validate([
+            'bookings' => 'required|array|min:1',
+            'bookings.*.bookable_id' => 'required|exists:bookables,id',
+            'bookings.*.from' => 'required|date|after_or_equal:today',
+            'bookings.*.to' => 'required|date|after_or_equal:bookings.*.from',
+            'customer.first_name' => 'required|min:2',
+            'customer.last_name' => 'required|min:2',
+            'customer.street' => 'required|min:2',
+            'customer.city' => 'required|min:2',
+            'customer.email' => 'required|email',
+            'customer.country' => 'required|min:2',
+            'customer.state' => 'required|min:2',
+            'customer.zip_code' => 'required|min:2',
+        ]);
+
+        $data = array_merge($data, $request->validate([
+            'bookings.*' => ['required', function ($attribute, $value, $fail) {
+                $bookable = Bookable::findOrFail($value['bookable_id']);
+                if (!$bookable->availableFor($value['from'], $value['to'])) {
+                    return $fail('The object is not available in given dates.');
+                }
+            }],
+        ]));
+
+        $bookingsData = $data['bookings'];
+        $addressData = $data['customer'];
+
+        $bookings = collect($bookingsData)->map(function ($bookingData) use ($addressData){
+            $booking = new Booking();
+            $booking->from = $bookingData['from'];
+            $booking->to = $bookingData['to'];
+            $booking->price = 200;
+            $booking->bookable_id = $bookingData['bookable_id'];
+
+            $booking->address()->associate(Address::create($addressData));
+
+            $booking->save();
+
+            return $booking;
+        });
+
+
+        return $bookings;
+    }
+}
+
